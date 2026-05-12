a30f1f1025e4eaf2cdb0bacaed57203f8f363d6d
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-05-12 10:49:35 -0500

    primer desarrollo del codigo

---
+++ ejercicio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int arreglo[] = {2,4,6,8,10};
+    int tam = 5;
+
+    for(int i = 0; i < tam; i++){
+        printf("%d ", arreglo[i]);  //se imprime el arreglo
+    }
+}
