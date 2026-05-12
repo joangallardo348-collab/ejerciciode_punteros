504ce40e7e581f59071d3f20844780b4eb11d455
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-05-12 11:05:32 -0500

    se le cambio el porcentaje d por p

--- ejercicio.c
+++ ejercicio.c
@@ -5,7 +5,7 @@ int main()
     int *ptr; //Introducimos punteros 
 
     ptr = &arreglo[0];
-    printf("%d ", ptr); 
+    printf("%p ", ptr); //se le cambio el porcentaje d por p 
     for(int i = 0; i < 5; i++){
         printf("%d ", arreglo[i]);  //se imprime el arreglo
     }
