d17aaeeac5178ef8e3e07308ce6dfd3073054c35
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-05-12 11:03:34 -0500

    introducimos punteros

--- ejercicio.c
+++ ejercicio.c
@@ -2,9 +2,11 @@
 int main()
 {
     int arreglo[] = {2,4,6,8,10};
-    int tam = 5;
+    int *ptr; //Introducimos punteros 
 
-    for(int i = 0; i < tam; i++){
+    ptr = &arreglo[0];
+    printf("%d ", ptr); 
+    for(int i = 0; i < 5; i++){
         printf("%d ", arreglo[i]);  //se imprime el arreglo
     }
 }
