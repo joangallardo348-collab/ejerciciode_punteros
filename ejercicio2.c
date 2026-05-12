05330de57c820124ae8611319e9725d6a07bc2ab
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-05-12 11:41:14 -0500

    primera opcion de impresion de vocales con punteros

--- ejercicio2.c
+++ ejercicio2.c
@@ -2,10 +2,13 @@
 int main()
 {
     char vocales [5] ={'a','e','i','o','u'};
-    
+    char *ptr;
+
+    ptr = &vocales[0];
 
     for(int i = 0; i < 5; i++)
     {
         printf("%c ", vocales[i]);
+        printf("%c\n", *(ptr+i));
     }
 }
