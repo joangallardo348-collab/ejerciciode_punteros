cf6e77133038d0bc4db1c027fa88d884a4239df3
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-05-12 11:11:33 -0500

    se imprime desde el origen

--- ejercicio.c
+++ ejercicio.c
@@ -5,8 +5,10 @@ int main()
     int *ptr; //Introducimos punteros 
 
     ptr = &arreglo[0];
-    printf("%p ", ptr); //se le cambio el porcentaje d por p 
+
+    //printf("%p ", ptr); //se le cambio el porcentaje d por p 
     for(int i = 0; i < 5; i++){
-        printf("%d ", arreglo[i]);  //se imprime el arreglo
+        printf("arreglo[%d] = %d ",i, arreglo[i]);  //se imprime el arreglo
+        printf("%d\n ", *ptr); 
     }
 }
