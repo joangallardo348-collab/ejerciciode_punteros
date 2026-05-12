8fd02bd86dae5fbeb4614c8d618349328e37ba19
Joan Gallardo <joan.gallardo@udla.edu.ec>
2026-05-12 11:14:14 -0500

    primera opcion de impresion de arreglos con punteros

--- ejercicio.c
+++ ejercicio.c
@@ -9,6 +9,7 @@ int main()
     //printf("%p ", ptr); //se le cambio el porcentaje d por p 
     for(int i = 0; i < 5; i++){
         printf("arreglo[%d] = %d ",i, arreglo[i]);  //se imprime el arreglo
+        ptr = &arreglo[i]; // primera opcion de impresion de arreglos con punteros
         printf("%d\n ", *ptr); 
     }
 }
